433334969af9c902400313f9fe2a9c8cba60fe57
Harald Glab-Plhak <hglabplhak@gmail.com>
2026-03-28 03:03:14 +0100

    work on a complete system with local research

--- extended/examples/RAG/AskByRAG.py
+++ extended/examples/RAG/AskByRAG.py
@@ -1,7 +1,13 @@
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_chroma import Chroma
 import os
 from pathlib import Path
+from langchain_core.embeddings import DeterministicFakeEmbedding
+#from langchain_community.embeddings import HuggingFaceEmbeddings
+#from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain.chat_models import init_chat_model
+from langchain_community.llms import Ollama
+from langchain.chains import RetrievalQA
 
 def get_app_key():
     fname = 'app_keyid.sec'
@@ -10,10 +16,11 @@ def get_app_key():
         f.close()
         return app_key
 
-def set_api_env_and_keys():
+def set_api_env_and_keys(mode):
     app_key = get_app_key()
     os.environ['LANGCHAIN_TRACING_V2'] = 'true'
-    os.environ['LANGCHAIN_ENDPOINT'] = 'https://withpersona.com/verify?inquiry-id=inq_NMrSJeR6Aiv2XciLNSjc5qcsv2vn'
+    if mode == "openai":
+        os.environ['LANGCHAIN_ENDPOINT'] = 'https://withpersona.com/verify?inquiry-id=inq_NMrSJeR6Aiv2XciLNSjc5qcsv2vn'
     os.environ['LANGCHAIN_API_KEY'] = app_key
     os.environ['OPENAI_API_KEY'] = app_key
     return
@@ -26,7 +33,8 @@ def get_dbpath():
     return db_path
 
 def get_vector_db():
-    embeddings = OpenAIEmbeddings()
+    embeddings = DeterministicFakeEmbedding(size=8192)
+    #embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     vector_db = Chroma(persist_directory=get_dbpath(), embedding_function=embeddings)
     return vector_db
 
@@ -38,25 +46,76 @@ def printout_retrieved_docs(docs):
     for doc in docs:
         print(f"Retrieved Document: {doc.page_content}")
 
+
+
+@tool(response_format="content_and_artifact")
+def retrieve_context(query: str):
+    """Retrieve information to help answer a query."""
+    retrieved_docs = vector_db.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\nContent: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
+
 def queryCompile(vector_db, query):
     answer = vector_db.similarity_search_with_score(query, k=2)
     retriever = vector_db.as_retriever(
         search_type="mmr",
         search_kwargs={"k": 3, "lambda_mult": 0.5}
     )
+   # client = vector_db._client
+    #embeddings = DeterministicFakeEmbedding(size=8192)
+    tools = [retrieve_context]
+    # If desired, specify custom instructions
+    prompt = (query)
+    model = init_chat_model("gpt-5.2")
 
+    #agent = create_agent(model, tools, system_prompt=prompt)
 
+    #for event in agent.stream(
+     #       {"messages": [{"role": "user", "content": query}]},
+     #       stream_mode="values",
+    #):
+     #   event["messages"][-1].pretty_print()
     # Use the retriever
     docs = retriever.invoke(query)
 
     return answer, docs
 
+def queryCompileLocal(vector_db, query):
+    answer = vector_db.similarity_search_with_score(query, k=2)
+    retriever = vector_db.as_retriever(
+        search_type="mmr",
+        search_kwargs={"k": 3, "lambda_mult": 0.5}
+    )
+    # 2. Connect to local LLM (Ollama)
+    llm = Ollama(model="llama3")
+    # 3. Create the chat/query chain
+    qa_chain = RetrievalQA.from_llm(llm=llm, retriever=vector_db.as_retriever())
+    # 4. Query your data
+    docs = qa_chain.invoke(query)
+
+    return answer, docs
+
+
 if __name__ == '__main__':
-    set_api_env_and_keys()
+    index = 0
+    mode = input("Query Mode :")
+    if mode == "openai":
+        set_api_env_and_keys(mode)
     vector_db = get_vector_db()
-    query = input("Prompt:")
+    query = input(f"Prompt({index}) :")
     while  query != 'exit':
-        answer, docs = queryCompile(vector_db, query)
+        if mode != 'openai' and mode != 'local':
+            print(f'Invalid mode :{mode} ')
+        if mode == "openai":
+            answer, docs = queryCompile(vector_db, query)
+        if mode == "local":
+            answer, docs = queryCompileLocal(vector_db, query)
+
         printout_results(answer)
+        print(docs)
        # printout_retrieved_docs(docs)
-        query = input("Next Prompt:")
+        index = index + 1
+        query = input(f"Next Prompt({index}) :")
---
+++ extended/examples/RAG/MyOwnRAG.py
@@ -0,0 +1,129 @@
+# %% [markdown]
+# 
+# %%
+import kagglehub
+#from AskByRAG import get_dbpath, set_api_env_and_keys
+from pathlib import Path
+from pypdf import PdfReader
+#from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_aws import BedrockEmbeddings
+from langchain_ollama import OllamaEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+import os
+#import faiss
+#from langchain_community.docstore.in_memory import InMemoryDocstore
+#from langchain_community.vectorstores import FAISS
+from langchain_core.embeddings import DeterministicFakeEmbedding
+
+dataset_of_pdf_files_path = kagglehub.dataset_download('manisha717/dataset-of-pdf-files')
+print(f"The dataset is loaded to the path: {dataset_of_pdf_files_path}")
+# %% [markdown]
+# 
+# %%
+
+# %%
+
+
+# %% [markdown]
+# 
+# %% [markdown]
+# 
+# %%
+def get_app_key():
+    fname = 'app_keyid.sec'
+    with open(fname) as f:
+        app_key = f.read()
+        f.close()
+        return app_key
+
+def set_api_env_and_keys():
+    app_key = get_app_key()
+    os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+    os.environ['LANGCHAIN_ENDPOINT'] = 'https://withpersona.com/verify?inquiry-id=inq_NMrSJeR6Aiv2XciLNSjc5qcsv2vn'
+    os.environ['LANGCHAIN_API_KEY'] = app_key
+    os.environ['OPENAI_API_KEY'] = app_key
+    return
+
+
+def get_dbpath():
+    home = Path.home()
+    db_path = os.path.join(home, 'chroma_vectordb')
+    if not os.path.exists(db_path):
+        os.makedirs(db_path)
+    return db_path
+# %% [markdown]
+# 
+# %%
+def build_vectors(complete_content):
+     # 2. Embed and Store in Vector DB (Chroma)
+    splitter = CharacterTextSplitter(
+                chunk_size = 2000,
+                chunk_overlap = 180,
+                )
+
+    chunks = splitter.create_documents(complete_content)
+
+    # 2. Embed and Store in Vector DB (Chroma)
+    #embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v2:0")
+    #embeddings = OllamaEmbeddings(model="llama3")
+    embeddings = DeterministicFakeEmbedding(size=8192)
+    vector_db = Chroma.from_documents(chunks, embedding=embeddings, persist_directory=get_dbpath())
+    return vector_db
+# %% [markdown]
+# 
+# %%
+def extract_text_from_pdf(file_path):
+    # creating a pdf reader object
+    reader = PdfReader(file_path)
+    content = ''
+# printing number of pages in pdf file
+    page_count = len(reader.pages)
+    print(f'Number of pages: {page_count}')
+    # getting a specific page from the pdf file
+    for index in range(page_count):
+        page = reader.pages[index]
+        text = page.extract_text()
+        content += text
+    return content
+# %% [markdown]
+# 
+# %%
+def read_all_docs(data_paths):
+    #absolute_path = data_path + '/Pdf'
+    docs_string = ''
+    for data_path in data_paths:
+        filenames = os.listdir(data_path)
+        for filename in filenames:
+            file_path = os.path.join(data_path, filename)
+            print(f"Processing {file_path}")
+            try:
+                text = extract_text_from_pdf(file_path)
+                docs_string += text
+            except Exception as e:
+                print(f"Error on {filename}: {e}")
+                docs_string += "Failed"
+                return -1, docs_string
+    return 0, [docs_string]
+
+# %% [markdown]
+# 
+# %%
+#docs_array = read_all_docs(dataset_of_pdf_files_path)
+if __name__ == '__main__':
+    set_api_env_and_keys()
+    absolute_path = dataset_of_pdf_files_path + '/Pdf'
+    ret_code, complete_content = read_all_docs(['/Users/hglabplhak/pdfdb', '/Users/hglabplhak/pdfprivdb'])
+    if ret_code == 0:
+        print(complete_content[0])
+        print(f"Count of docs: {len(complete_content)}")
+        print("build vector")
+        vector_db = build_vectors(complete_content)
+        print('ready')
+# %% [markdown]
+# 
+# %%
+
+# %% [markdown]
+# 
